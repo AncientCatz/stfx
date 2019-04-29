97d64897388e19952852a5e8234233e92284af3a
Taylor Swift <consulting@taylorswift.com>
2019-04-29 17:37:00 +0200

    yolo

--- code.js
+++ code.js
@@ -1,4 +1,61 @@
 protobuf.load('proto.json').then(x => proto = x)
+function CopyToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a
+  // flash, so some of these are just precautions. However in
+  // Internet Explorer the element is visible whilst the popup
+  // box asking the user for permission for the web page to
+  // copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+};
 
 String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
 function () {
@@ -211,7 +268,8 @@ Vue.component('crew-wizard', {
                 'navigation': 0
             }
             for(var i in crew_stats) {
-                crewman = {'job': this.jobs[stat_job[i]], 'level': this.level}
+                level = Number(this.level)
+                crewman = {'job': this.jobs[stat_job[i]], 'level': level}
                 crewman_skills = this.crew_get_skills(crewman)
                 crewman_skill = crewman_skills[i]
 
@@ -223,7 +281,7 @@ Vue.component('crew-wizard', {
                 for(var cs in crewman_skills) {
                     crew_stats[cs] += crewman_skills[cs]
                 }
-                new_crew.push({'job': crewman.job, 'count': crew_no, 'level':this.level})
+                new_crew.push({'job': crewman.job, 'count': crew_no, 'level': level})
             }
             this.new_crew = new_crew
         },
@@ -253,6 +311,7 @@ Vue.component('crew-table', {
             job_dropdown_current: null,
             new_crewman_job: null,
             new_crewman_level: null,
+            global_crewman_level: null,
         }
     },
     watch: {
@@ -261,7 +320,14 @@ Vue.component('crew-table', {
             this.new_crewman_job = null
             new_crew = [{'job': job, 'count': 1, 'level': 1}]
             bus.$emit(EVT_FILL_SHIP_POOLS, new_crew)
-        }
+        },
+        global_crewman_level: function(){
+            if(!this.global_crewman_level){return;}
+            for(var i in this.crew) {
+                this.crew[i].level = this.global_crewman_level;
+            }
+            this.global_crewman_level = null;
+        },
     },
     methods: {
         get_dropdown_jobs : function() {
@@ -441,6 +507,7 @@ var app = new Vue({
             console.log(data_c)
             data_base = base64js.fromByteArray(data_c)
             document.location.hash = data_base
+            CopyToClipboard(document.location.href);
         },
         stfx_import: function(data_base) {
             data_c = base64js.toByteArray(data_base)
