65e0d31567d2486c1d3c04d951cd90fdd9894a01
Taylor Swift <consulting@taylorswift.com>
2019-08-04 18:36:45 +0200

    drop intermediary jsons; download component overlays

--- dist/assets/assetgetter.py
+++ dist/assets/assetgetter.py
@@ -3,6 +3,19 @@ from lxml.html import fromstring
 import requests
 import os
 
+
+with open('../data/data.json') as f:
+    data = json.load(f)
+
+
+componentnames = list(set(d['img'] for d in data['components'].values()))
+craftnames = list(set(d['img'] for d in data['crafts'].values()))
+talentnames = list(set(d['icon'] for d in data['talents'].values()))
+shipnames = list(set(d['skin_name'] for d in data['ships'].values()))
+compmodnames = list(set(d['imgmod'] for d in data['components'].values() if 'imgmod' in d))
+print(shipnames)
+
+
 def get_jobs():
     page = requests.get('https://startraders.gamepedia.com/Star_Traders_Wiki')
     tree = fromstring(page.text)
@@ -12,29 +25,38 @@ def get_jobs():
         name = job[-1].get('title').lower().replace(' ', '_') + '.png'
         img_src = job[0][0].get('src')
 
+        if os.path.exists(f'jobs/{name}'):
+            print('exists')
+            continue
         img = requests.get(img_src)
         with open(f'jobs/{name}', 'wb') as f:
             f.write(img.content)
         print(name, img)
 
+
 def get_components():
-    with open("./componentnames.json") as f:
-        components = json.load(f)
-    magic_getter(components, 'components')
+    magic_getter(componentnames, 'components')
+
+def get_component_mods():
+    magic_getter(compmodnames, 'component_mods')
+
 
 def get_crafts():
-    with open("./craftnames.json") as f:
-        crafts = json.load(f)
-    magic_getter(crafts, 'crafts')
-    magic_getter(crafts, 'crafts_top', lambda s: f"{s}_top")
+    magic_getter(craftnames, 'crafts')
+    magic_getter(craftnames, 'crafts_top', lambda s: f"{s}_top")
+
+
+def get_ships():
+    magic_getter(shipnames, 'ships')
+    magic_getter(shipnames, 'ships_top', lambda s: f"{s}_top")
 
 
 def get_talents():
-    with open("./talentnames.json") as f:
-        talentnames = json.load(f)
     magic_getter(talentnames, 'talents', lambda n: n +'_normal')
 
 def magic_getter(png_list, directory, func=lambda n: n):
+    if not os.path.exists(f'{directory}'):
+        os.mkdir(directory)
     for p in png_list:
         print(p)
         if os.path.exists(f'{directory}/{p}.png'):
@@ -53,16 +75,15 @@ def magic_getter(png_list, directory, func=lambda n: n):
 
 
 
-def get_ships():
-    with open("./shipnames.json") as f:
-        shipnames = json.load(f)
+def main():
+    get_talents()
+    get_jobs()
+    get_talents()
+    get_ships()
+    get_components()
+    get_component_mods()
+    get_crafts()
 
-    magic_getter(shipnames, 'ships')
-    magic_getter(shipnames, 'ships_top', lambda s: f"{s}_top")
-# get_talents()
+if __name__ == '__main__':
+    main()
 
-# get_jobs()
-get_talents()
-get_ships()
-get_components()
-get_crafts()
